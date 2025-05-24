e3dd08b7a323463f6494998aae588f45e579eac3
Markus Johannsen <markus.johannsen@mjoh.io>
2025-05-24 10:09:31 +0200

    remove imports

--- src/main/java/io/mjoh/camunda/worker/ExternalWorkerApp.java
+++ src/main/java/io/mjoh/camunda/worker/ExternalWorkerApp.java
@@ -1,19 +1,13 @@
 package io.mjoh.camunda.worker;
 
-import io.mjoh.camunda.handler.ExternalTaskHandler;
 import io.mjoh.camunda.configuration.ExternalTaskProperties;
+import io.mjoh.camunda.handler.ExternalTaskHandler;
 import jakarta.annotation.PostConstruct;
 import org.camunda.bpm.client.ExternalTaskClient;
-import org.camunda.bpm.client.task.ExternalTask;
-import org.camunda.bpm.client.task.ExternalTaskService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExternalWorkerApp {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExternalWorkerApp.class);
 
     private final ExternalTaskClient externalTaskClient;
     private final ExternalTaskProperties externalTaskProperties;
